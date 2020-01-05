ed1323333954e9e4cd853839736a977277fdb2c6
ScorpionOrange <scorpionorange@126.com>
2020-01-06 00:37:38 +0800

    update at 2020.1.06 12:37am

--- app/src/main/java/com/example/calculator2_1_1injava/MainActivity.java
+++ app/src/main/java/com/example/calculator2_1_1injava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator2_1_1injava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -39,5 +40,140 @@ public class MainActivity extends AppCompatActivity {
         btPlus = (Button) findViewById(R.id.buttonPlus);
         btEqual = (Button) findViewById(R.id.buttonEqual);
         btC = (Button) findViewById(R.id.buttonC);
+
+        String i = Integer.toString(x);
+        etInOut.setText(i);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(1);
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(2);
+            }
+        });
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(3);
+            }
+        });
+
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(4);
+            }
+        });
+
+        bt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(5);
+            }
+        });
+
+        bt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(6);
+            }
+        });
+
+        bt7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(7);
+            }
+        });
+
+        bt8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(8);
+            }
+        });
+
+        bt9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(9);
+            }
+        });
+
+        bt0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settext(0);
+            }
+        });
+
+        btC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clean();
+            }
+        });
+
+        btPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setoperator("+");
+            }
+        });
+
+        btEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equal();
+            }
+        });
+    }
+
+    private void settext(int input){
+        String i = Integer.toString(input);
+        if(sign == 0){
+            etInOut.setText(i);
+            x = input;
+            sign = 1;
+        } else if (sign == 2){
+            etInOut.setText(i);
+            y = input;
+        }
+    }
+
+    private void clean(){
+        etInOut.setText("0");
+        etShowInfo.setText("");
+        x = 0;
+        y = 0;
+        result = 0;
+        sign = 0;
+    }
+
+    private void setoperator(String op){
+        if(sign == 1 || sign == 0){
+            operator = op;
+            etShowInfo.setText(etInOut.getText() + op);
+            etInOut.setText("");
+            sign = 2;
+        }
+    }
+
+    private void equal(){
+        if(operator.equals("+") && sign == 2){
+            result = x + y;
+            etShowInfo.setText(x + operator + y);
+            etInOut.setText("" + result);
+            sign = 0;
+            operator = "";
+            x = result;
+        }
     }
 }
