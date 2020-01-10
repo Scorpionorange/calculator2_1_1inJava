a16b481d3f54808ddac4e25bbe328cfd7d2a3579
ScorpionOrange <scorpionorange@126.com>
2020-01-11 01:19:06 +0800

    update at 2020.1.11 1:12am

--- app/src/main/java/com/example/calculator2_1_1injava/MainActivity.java
+++ app/src/main/java/com/example/calculator2_1_1injava/MainActivity.java
@@ -250,16 +250,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void clean(){
-        etInOut.setText("0");
+        etInOut.setText("0.0");
         etShowInfo.setText("");
         x = 0;
         y = 0;
+        op1 = "";
+        op2 = "";
         result = 0;
         sign = 0;
+        pointsign = 0;
     }
 
-    private void setoperator(String op){
-        if(sign == 1 || sign == 0){
+    private void setoperator(String op) {
+        if (pointsign == 1)
+            pointsign = 0;
+        if (sign == 0 || sign == 1) {
             operator = op;
             etShowInfo.setText(etInOut.getText() + op);
             etInOut.setText("");
@@ -267,14 +272,61 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void equal(){
-        if(operator.equals("+") && sign == 2){
-            result = x + y;
+    private void equal() {
+        if (operator.equals("÷") && sign == 2){
+            if (y != 0 && y != 0.0) {
+                result = x / y;
+            } else {
+                etInOut.setText("非法计算");
+                return;
+            }
             etShowInfo.setText(x + operator + y);
             etInOut.setText("" + result);
             sign = 0;
             operator = "";
             x = result;
         }
+        op1 = "";
+        op2 = "";
     }
+
+    private void delete() {
+        double i;
+        // TODO Auto-generated method stub
+        if (sign == 1) {
+            if (op1.length() - 1 > 0) {
+                char pointcheck=op1.charAt(op1.length()-1);
+                if(pointcheck=='.')pointsign=0;
+                op1 = op1.substring(0, op1.length() - 1);
+                etInOut.setText(op1);
+                i = Double.parseDouble(op1);
+                x = i;
+            } else {
+                op1 = "0.0";
+                etInOut.setText(op1);
+                i = Double.parseDouble(op1);
+                x = i;
+                op1 = "";
+                sign = 0;
+            }
+        } else if (sign == 2) {
+            if (op2.length() - 1 > 0) {
+                char pointcheck=op2.charAt(op2.length()-1);
+                if(pointcheck=='.')pointsign=0;
+                op2 = op2.substring(0, op2.length() - 1);
+                etInOut.setText(op2);
+                i = Double.parseDouble(op2);
+                y = i;
+            } else {
+                op2 = "0.0";
+                etInOut.setText(op2);
+                i = Double.parseDouble(op2);
+                y = i;
+                op2 = "";
+                sign = 0;
+            }
+
+        }
+    }
+
 }
